92490513517d3eee8213df742b5d3ad7bb03fac5
OrlandoLeyvaF <orlandoleyvafontes@gmail.com>
2025-02-15 21:36:47 -0700

    Tercer Comit
    -Se creo la main
    -La carpeta de presentacion tiene parte de catalogo(todavía no se puede agregar las ciudades)

---
+++ RetoCinePresentacion/src/main/java/Main/main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Presentacion.MenuAdministrador;
+import dto.CiudadesDTO;
+import inegocio.ICuidadesNegocio;
+import negocio.CiudadesNegocio;
+
+/**
+ *
+ * @author Oley
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    
+    CiudadesDTO ciudadesDTO=new CiudadesDTO();
+    
+    ICuidadesNegocio cuidadesNegocio=new CiudadesNegocio();
+    
+    
+    MenuAdministrador menuAdministrador=new MenuAdministrador();
+    menuAdministrador.setVisible(true);
+}
+}
